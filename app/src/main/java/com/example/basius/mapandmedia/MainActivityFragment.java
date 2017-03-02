5aab43d980c14713f9af6bc3cb103c6e6fb69d3f
basius <jorabada@gmail.com>
2017-03-02 16:24:56 +0100

    Comprovem que el gps estigui activat, cas contrari demanem que s'activi

---
+++ app/src/main/java/com/example/basius/mapandmedia/MainActivityFragment.java
@@ -0,0 +1,99 @@
+package com.example.basius.mapandmedia;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+    MapView map;
+
+    private MyLocationNewOverlay myLocationOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private CompassOverlay mCompassOverlay;
+    private IMapController mapController;
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        map = (MapView) view.findViewById(R.id.map);
+        initializeMap();
+        setZoom();
+        setOverlays();
+        map.invalidate();
+
+        return view;
+    }
+    private void initializeMap() {
+        //Comprovem que el gps estigui activat
+        String locationProviders = Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if (locationProviders == null || locationProviders.equals("")) {
+            GPSTracker gps = new GPSTracker(getContext());
+            gps.showSettingsAlert();
+        }
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setTilesScaledToDpi(true);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+    }
+    private void setZoom() {
+        //  Setteamos el zoom al mismo nivel y ajustamos la posición a un geopunto
+        mapController = map.getController();
+        mapController.setZoom(15);
+    }
+
+    private void setOverlays() {
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+
+        myLocationOverlay = new MyLocationNewOverlay(
+                new GpsMyLocationProvider(getContext()),
+                map
+        );
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo( myLocationOverlay
+                        .getMyLocation());
+            }
+        });
+
+        mScaleBarOverlay = new ScaleBarOverlay(map);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mCompassOverlay = new CompassOverlay(
+                getContext(),
+                new InternalCompassOrientationProvider(getContext()),
+                map
+        );
+        mCompassOverlay.enableCompass();
+
+        map.getOverlays().add(myLocationOverlay);
+        map.getOverlays().add(this.mScaleBarOverlay);
+        map.getOverlays().add(this.mCompassOverlay);
+    }
+}
